86a511162f251c3d0fab479fe0c6dcc5ad534ab0
Sapphire <70076973+varSapphire@users.noreply.github.com>
2022-10-28 21:07:06 -0500

    Update commands.go

--- commands.go
+++ commands.go
@@ -5,10 +5,52 @@ import "github.com/bwmarrin/discordgo"
 var dmPermission bool = false
 
 var commands = []*discordgo.ApplicationCommand{
-	// Hello, world! command.
 	{
-		Name:         "hello",
-		Description:  "Hello, world!",
-		DMPermission: &dmPermission,
+		Name:        "register",
+		Description: "This command registers you as a member of your system so that you can proxy messages.",
+
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "name",
+				Description: "Your name.",
+				Required:    true,
+				MaxLength:   64,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "identifier",
+				Description: "The unique identifier used to proxy messages.",
+				Required:    true,
+				MaxLength:   16,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "avatar",
+				Description: "The avatar used by you.",
+				Required:    true,
+			},
+		},
+	},
+	{
+		Name:        "proxy",
+		Description: "This command will proxy a member of your system using their identifier.",
+
+		Options: []*discordgo.ApplicationCommandOption{
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "identifier",
+				Description: "Your identifier.",
+				Required:    true,
+				MaxLength:   16,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionString,
+				Name:        "message",
+				Description: "The message that you wish to send.",
+				Required:    true,
+				MaxLength:   2000,
+			},
+		},
 	},
 }
