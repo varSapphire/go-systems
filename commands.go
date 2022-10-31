cd0867720f1309ca8abbcc37213b3d68350af29b
Sapphire <quinton.oglesby@icloud.com>
2022-10-31 15:44:53 -0500

    Update commands.go

--- commands.go
+++ commands.go
@@ -3,8 +3,15 @@ package main
 import "github.com/bwmarrin/discordgo"
 
 var dmPermission bool = false
+var manageServerPermission int64 = discordgo.PermissionManageServer
 
 var commands = []*discordgo.ApplicationCommand{
+	{
+		Name:                     "setup",
+		Description:              "This command creates a channel for logging.",
+		DefaultMemberPermissions: &manageServerPermission,
+		DMPermission:             &dmPermission,
+	},
 	{
 		Name:        "register",
 		Description: "This command registers you as a member of your system so that you can proxy messages.",
@@ -33,7 +40,7 @@ var commands = []*discordgo.ApplicationCommand{
 		},
 	},
 	{
-		Name:        "proxy",
+		Name:        "proxy_member",
 		Description: "This command will proxy a member of your system using their identifier.",
 
 		Options: []*discordgo.ApplicationCommandOption{
@@ -48,8 +55,68 @@ var commands = []*discordgo.ApplicationCommand{
 				Type:        discordgo.ApplicationCommandOptionString,
 				Name:        "message",
 				Description: "The message that you wish to send.",
-				Required:    true,
+				Required:    false,
 				MaxLength:   2000,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_1",
+				Description: "The first optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_2",
+				Description: "The second optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_3",
+				Description: "The third optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_4",
+				Description: "The fourth optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_5",
+				Description: "The fifth optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_6",
+				Description: "The sixth optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_7",
+				Description: "The seventh optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_8",
+				Description: "The eighth optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_9",
+				Description: "The fourth optional attachment.",
+				Required:    false,
+			},
+			{
+				Type:        discordgo.ApplicationCommandOptionAttachment,
+				Name:        "attachment_10",
+				Description: "The tenth optional attachment.",
+				Required:    false,
 			},
 		},
 	},
