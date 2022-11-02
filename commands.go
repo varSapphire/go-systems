46163548a5889041ad7dfbe733c5ed95c276082d
Sapphire <quinton.oglesby@icloud.com>
2022-11-02 07:54:59 -0500

    Update commands.go

--- commands.go
+++ commands.go
@@ -4,6 +4,8 @@ import "github.com/bwmarrin/discordgo"
 
 var dmPermission bool = false
 var manageServerPermission int64 = discordgo.PermissionManageServer
+var tokenMinLength int = 64
+var tokenMaxLength int = 64
 
 var commands = []*discordgo.ApplicationCommand{
 	{
@@ -13,110 +15,18 @@ var commands = []*discordgo.ApplicationCommand{
 		DMPermission:             &dmPermission,
 	},
 	{
-		Name:        "register",
-		Description: "This command registers you as a member of your system so that you can proxy messages.",
+		Name:         "register",
+		Description:  "This command registers your PluralKit token into the database.",
+		DMPermission: &dmPermission,
 
 		Options: []*discordgo.ApplicationCommandOption{
 			{
 				Type:        discordgo.ApplicationCommandOptionString,
-				Name:        "name",
-				Description: "Your name.",
+				Name:        "token",
+				Description: "Your PluralKit token.",
 				Required:    true,
-				MaxLength:   64,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionString,
-				Name:        "identifier",
-				Description: "The unique identifier used to proxy messages.",
-				Required:    true,
-				MaxLength:   16,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "avatar",
-				Description: "The avatar used by you.",
-				Required:    true,
-			},
-		},
-	},
-	{
-		Name:        "proxy_member",
-		Description: "This command will proxy a member of your system using their identifier.",
-
-		Options: []*discordgo.ApplicationCommandOption{
-			{
-				Type:        discordgo.ApplicationCommandOptionString,
-				Name:        "identifier",
-				Description: "Your identifier.",
-				Required:    true,
-				MaxLength:   16,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionString,
-				Name:        "message",
-				Description: "The message that you wish to send.",
-				Required:    false,
-				MaxLength:   2000,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_1",
-				Description: "The first optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_2",
-				Description: "The second optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_3",
-				Description: "The third optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_4",
-				Description: "The fourth optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_5",
-				Description: "The fifth optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_6",
-				Description: "The sixth optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_7",
-				Description: "The seventh optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_8",
-				Description: "The eighth optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_9",
-				Description: "The fourth optional attachment.",
-				Required:    false,
-			},
-			{
-				Type:        discordgo.ApplicationCommandOptionAttachment,
-				Name:        "attachment_10",
-				Description: "The tenth optional attachment.",
-				Required:    false,
+				MinLength:   &tokenMinLength,
+				MaxLength:   tokenMaxLength,
 			},
 		},
 	},
