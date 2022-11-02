f6d053b1da0e690586893694f8a9111a99f037ea
Sapphire <quinton.oglesby@icloud.com>
2022-11-02 10:00:48 -0500

    Update helpers.go

--- helpers.go
+++ helpers.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"github.com/bwmarrin/discordgo"
+	"log"
+)
+
 var (
 	Reset  = "\033[0m"
 	Bold   = "\033[1m"
@@ -12,3 +18,58 @@ var (
 	Gray   = "\033[37m"
 	White  = "\033[97m"
 )
+
+func checkForWebhook(session *discordgo.Session, interaction *discordgo.Interaction) (webhookFlag bool, webhookID string, webhookToken string) {
+	webhookFlag = false
+	webhookID = ""
+	webhookToken = ""
+
+	webhooks, err := session.ChannelWebhooks(interaction.ChannelID)
+	if err != nil {
+		log.Printf("%vERROR%v - COULD NOT RETRIEVE LIST OF WEBHOOKS FROM CHANNEL:\n\t%v", Red, Reset, err.Error())
+
+		content := fmt.Sprintf("ERROR - COULD NOT RETRIEVE LIST OF WEBHOOKS FROM CHANNEL:\n\t%v", err.Error())
+		session.InteractionResponseEdit(interaction, &discordgo.WebhookEdit{Content: &content})
+
+		return webhookFlag, webhookID, webhookToken
+	}
+
+	for _, webhook := range webhooks {
+		if webhook.Name == "All Systems Go Proxy Webhook" {
+			webhookFlag = true
+			webhookID = webhook.ID
+			webhookToken = webhookToken
+
+			return webhookFlag, webhookID, webhookToken
+		}
+	}
+
+	return false, "", ""
+}
+
+func checkForMessage(interaction *discordgo.Interaction) (memberMessage string, messageFlag bool) {
+	for _, option := range interaction.ApplicationCommandData().Options {
+		if option.Name == "message" {
+			memberMessage = option.StringValue()
+			messageFlag = true
+
+			return memberMessage, messageFlag
+		}
+	}
+
+	return "", false
+}
+
+func checkforLogging(session *discordgo.Session, interaction *discordgo.Interaction) (loggingChannelID string, loggingFlag bool) {
+	channels, _ := session.GuildChannels(interaction.GuildID)
+	for _, channel := range channels {
+		if channel.Name == "asg-logs" {
+			loggingChannelID = channel.ID
+			loggingFlag = true
+
+			return loggingChannelID, loggingFlag
+		}
+	}
+
+	return "", false
+}
