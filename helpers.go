1dc565a4beeb7cd3042078c6fa0011bbad3bcd6b
Sapphire <quinton.oglesby@icloud.com>
2022-11-02 07:55:09 -0500

    Update helpers.go

--- helpers.go
+++ helpers.go
@@ -1,10 +1,5 @@
 package main
 
-import (
-	"github.com/bwmarrin/discordgo"
-	"log"
-)
-
 var (
 	Reset  = "\033[0m"
 	Bold   = "\033[1m"
@@ -17,20 +12,3 @@ var (
 	Gray   = "\033[37m"
 	White  = "\033[97m"
 )
-
-func WebhookExecuteSimple(
-	session *discordgo.Session,
-	webhookID string,
-	webhookToken string,
-	memberMessage string,
-	memberName string,
-	memberAvatarURL string) {
-	_, err = session.WebhookExecute(webhookID, webhookToken, true, &discordgo.WebhookParams{
-		Content:   memberMessage,
-		Username:  memberName,
-		AvatarURL: memberAvatarURL,
-	})
-	if err != nil {
-		log.Printf("%vERROR%v - COULD NOT EXECUTE WEBHOOK:\n\t%v", Red, Reset, err.Error())
-	}
-}
